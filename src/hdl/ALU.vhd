acff2374fa5eb730840b18d20bd4e3701f8daba3
mrwizard26 <141947831+mrwizard26@users.noreply.github.com>
2024-05-06 14:38:15 -0600

    finished ALU

--- src/hdl/ALU.vhd
+++ src/hdl/ALU.vhd
@@ -49,19 +49,33 @@ architecture behavioral of ALU is
   
 	-- declare components and signals
     signal w_adder : STD_LOGIC_VECTOR(8 DOWNTO 0);
+    signal w_and_nand : STD_LOGIC_VECTOR(8 DOWNTO 0);
+    signal w_or_nor : STD_LOGIC_VECTOR(8 DOWNTO 0);
+    signal w_shift : STD_LOGIC_VECTOR(8 DOWNTO 0);
     signal w_output : STD_LOGIC_VECTOR(7 DOWNTO 0);
 
 
     
 begin
-       w_adder <= std_logic_vector(unsigned('0'&i_A) + unsigned('0'&i_B));
+       w_adder <= std_logic_vector(unsigned('0'&i_A) + unsigned('0'&i_B)) when i_op(2) = '0' else
+                  std_logic_vector(unsigned('0'&i_A) - unsigned('0'&i_B));
+       w_and_nand <= i_A AND i_B when i_op(2) = '0' else
+                     NOT(i_A AND i_B);
+       w_or_nor <= i_A OR i_B when i_op(2) = '0' else
+                   NOT(i_A NOR i_B);
+     
+       w_shift <= std_logic_vector(shift_left(unsigned(i_A), to_integer(unsigned(i_B(2 downto 0))))) when i_op(2) = '0' else
+                  std_logic_vector(shift_right(unsigned(i_A), to_integer(unsigned(i_B(2 downto 0)))));
        
        --w_adder <= std_logic_vector(unsigned(i_A) + unsigned(i_B));
-       w_output <= w_adder(7 DOWNTO 0);
+       w_output <= w_adder(7 DOWNTO 0) when i_op(1 DOWNTO 0) = "00" else
+                   w_and_nand(7 DOWNTO 0) when i_op(1 DOWNTO 0) = "01" else
+                   w_or_nor(7 DOWNTO 0) when i_op(1 DOWNTO 0) = "10" else
+                   w_shift(7 DOWNTO 0);
 	   o_results <= w_output;
-	   o_flags(2 DOWNTO 1) <= "00";
-	   o_flags(0) <= w_adder(8);
-	   --o_flags(1) <= '1' when (w_output = "00000000") else '0'; -- zero flag
+	   o_flags(2) <= '1' when (w_output(7) = '1') else '0';
+	   o_flags(0) <= w_adder(8); -- C out
+	   o_flags(1) <= '1' when (w_output = "00000000") else '0'; -- zero flag
 	   
 	
 	
